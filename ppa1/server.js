d104a2db86ba61a670f91b75b5799117d57018d7
Eric Chang <changyungta2001@gmail.com>
2026-02-05 18:48:38 -0600

    feat: add ppa1

---
+++ ppa1/server.js
@@ -0,0 +1,30 @@
+import http from "http";
+import fs from "fs";
+
+const server = http.createServer((req, res) => {
+  let filePath = "./public/index.html";
+
+  // Routing Logic
+  if (req.url === "/provider") {
+    filePath = "./public/provider.html";
+  }
+  if (req.url === "/client") {
+    filePath = "./public/client.html";
+  }
+
+  // File Serving Logic
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      res.writeHead(500);
+      res.end("Server error");
+      return;
+    }
+    
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(content);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Server running at http://localhost:3000");
+});
